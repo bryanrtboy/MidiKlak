13832b177cbc7259cf7e36d96897cfee3e94ae2a
bryanrtboy <bryan@bryanleister.com>
2016-06-27 11:08:01 -0700

    Updated Input to New Classes
    Instead of overwriting Klak-Midi, created new classes that work with the
    Node based Klak system. Requires only Klak and MIDJack.

---
+++ Assets/Klak-Midi-Learn/Scripts/SetCC.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using MidiJack;
+
+
+namespace Klak.Midi
+{
+	[RequireComponent (typeof(KnobLearnInput))]
+	public class SetCC : MonoBehaviour
+	{
+
+		//What the name of this knob corresponds to
+		public StoredCCValue.SavedName m_name;
+
+		KnobLearnInput m_knob;
+
+		void Awake ()
+		{
+			m_knob = GetComponent<KnobLearnInput> () as KnobLearnInput;
+		}
+
+		void Start ()
+		{
+			if (PlayerPrefs.HasKey (m_name.ToString ()))
+				m_knob.SetKnobNumberTo (PlayerPrefs.GetInt (m_name.ToString ()));
+		}
+	}
+
+	[Serializable]
+	public class StoredCCValue
+	{
+
+		public enum SavedName
+		{
+			Knob1,
+			Knob2,
+			Knob3,
+			Knob4,
+			Knob5,
+			Knob6,
+			Knob7,
+			Knob8
+		}
+
+		[SerializeField]
+		public SavedName name = SavedName.Knob1;
+	}
+}
---
+++ Assets/Klak-Midi-Learn/Scripts/SetNote.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using MidiJack;
+
+
+namespace Klak.Midi
+{
+	[RequireComponent (typeof(NoteLearnInput))]
+	public class SetNote : MonoBehaviour
+	{
+
+		//What the name of this knob corresponds to
+		public StoredNoteValue.SavedName m_name;
+
+		NoteLearnInput m_noteInput;
+
+		void Awake ()
+		{
+			m_noteInput = GetComponent<NoteLearnInput> () as NoteLearnInput;
+		}
+
+		void Start ()
+		{
+			if (PlayerPrefs.HasKey (m_name.ToString ()))
+				m_noteInput.SetNoteNumberTo (PlayerPrefs.GetInt (m_name.ToString ()));
+		}
+	}
+
+	[Serializable]
+	public class StoredNoteValue
+	{
+
+		public enum SavedName
+		{
+			Button1,
+			Button2,
+			Button3,
+			Button4,
+			Button5,
+			Button6,
+			Button7,
+			Button8
+		}
+
+		[SerializeField]
+		public SavedName name = SavedName.Button1;
+	}
+}
